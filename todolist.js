3245000c4abdfbb5495d096324d6afd8e34021da
rohangawhade <49246157+rohangawhade@users.noreply.github.com>
2020-04-22 02:03:35 +0530

    Initial commit

---
+++ todolist.js
@@ -0,0 +1,21 @@
+$("ul").on('click','li',function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on('click','span',function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which == 13){
+        var TEXT = $(this).val();
+        $(this).val('');
+        $('ul').append("<li><span><i class='fas fa-trash'></i></span> "+TEXT+"</li>");
+    }
+});
+$("h1").on('click','i',function(){
+    $('input[type="text"]').fadeToggle(0);
+})
